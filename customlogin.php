4297390a4d892a6fe77fb0edf26a19732a4be120
alixiazul <alicia.rodriguez.t@gmail.com>
2023-03-22 14:08:29 +0000

    init

---
+++ customlogin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name:  AR Custom Login
+Description:  Plugin which creates a custom login page
+Plugin URI:   https://aliciarodriguezweb.com
+Author:       Alicia Rodriguez
+Version:      1.0
+Text Domain:  arcustomlogin
+Domain Path:  /languages
+License:      GPL v2 or later
+License URI:  https://www.gnu.org/licenses/gpl-2.0.txt
+*/
