2ca177717be2df99c1f6cce3ef7b0aeed236db63
alixiazul <alicia.rodriguez.t@gmail.com>
2023-03-23 14:39:43 +0000

    Adding a sublevel menu item in general settings

--- customlogin.php
+++ customlogin.php
@@ -60,7 +60,7 @@ function arcustomlogin_display_settings_page() {
 // 			callable $function = '', 
 // 			string   $icon_url = '', 
 // 			int      $position = null 
-// 		)
+// 		);
 // 	*/
 
 //     add_menu_page(
@@ -76,3 +76,29 @@ function arcustomlogin_display_settings_page() {
 
 // // Registration of the function with the "admin_menu" action hook
 // add_action( 'admin_menu', 'arcustomlogin_add_toplevel_menu' );
+
+function arcustomlogin_add_sublevel_menu() {
+
+    /*	
+	add_submenu_page(
+		string   $parent_slug,
+		string   $page_title,
+		string   $menu_title,
+		string   $capability,
+		string   $menu_slug,
+		callable $function = ''
+	);	
+	*/
+
+    add_submenu_page(
+        'options-general.php',
+        'Custom Login Settings',
+        'Custom Login',
+        'manage_options',
+        'arcustomlogin',
+        'arcustomlogin_display_settings_page'
+    );
+}
+
+// Registration of the function with the "admin_menu" action hook
+add_action( 'admin_menu', 'arcustomlogin_add_sublevel_menu' );
