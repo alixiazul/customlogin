1bf43f75c1c38ce884394641283297ca0c5dd09b
alixiazul <alicia.rodriguez.t@gmail.com>
2023-03-26 19:24:19 +0100

    Radio field call back function added.

--- admin/settings-callbacks.php
+++ admin/settings-callbacks.php
@@ -48,8 +48,45 @@ function arcustomlogin_callback_field_text( $args ) {
 }
 
 // Call back: radio field
-function arcustomlogin_callback_field_radio() {
-    echo 'This will be a radio field';
+function arcustomlogin_callback_field_radio( $args ) {
+    // 1: Defining variables
+
+    // Options API
+
+    // Getting the plugin options from the database. 
+    // 1st argument: The name of the option itself. We use this name when retrieving the option from the database. It is defined in the "register_setting", 
+    // as the 2nd parameter
+    // 2nd argument: default options to use in case the options are not found in the database
+    $options = get_option( 'arcustomlogin_options', arcustomlogin_default_options() );
+
+    // The arguments should match the ones set in the settings-register.php for each setting field
+    $id     = isset($args['id'])    ? $args['id']    : '';
+    $label  = isset($args['label']) ? $args['label'] : '';
+
+    // Get the value for the radio field that we are displaying
+    $selected_option = isset($options[$id]) ? sanitize_text_field( $options[$id] ) : '';
+
+    // Get all the radio options available
+    $radio_options = array(
+        // value    =>  label
+        'enable'    => 'Enable custom styles',
+        'disable'   => 'Disable custom styles'
+    );
+
+    // ------------------------------------------------------------------
+
+    // 2: Output the radio field markup
+    foreach( $radio_options as $value => $label ) {
+        // checked ( $checked, $current, $echo)
+        // $checked = one of the values to compare
+        // $current = the other value to compare (default = true)
+        // $echo    = whether to echo the result or just return the string (default = true)    
+        
+        $checked = checked( $selected_option === $value, true, false ); // Not echoing but returning
+        
+        echo '<input id="arcustomlogin_option_'. $id .'" name="arcustomlogin_options['. $id .']" type="radio" value="'. $value .'" ' . $checked . '>';
+        echo '<label for="arcustomlogin_options_'. $id .'">'. $label .'</label><br/>';
+    }
 }
 
 // Call back: textarea field
