25af5d1e55bdf6312ebecaa31bba7a3eccbb09e2
alixiazul <alicia.rodriguez.t@gmail.com>
2023-03-26 09:24:04 +0100

    Callback functions for all fields added.

--- customlogin.php
+++ customlogin.php
@@ -178,3 +178,28 @@ function arcustomlogin_callback_section_login() {
 function arcustomlogin_callback_section_admin() {
     echo '<p>These settings enable you to customize the WP Admin Area.</p>';
 }
+
+// Call back: field text
+function arcustomlogin_callback_field_text() {
+    echo 'This will be a text field';
+}
+
+// Call back: radio field
+function arcustomlogin_callback_field_radio() {
+    echo 'This will be a radio field';
+}
+
+// Call back: textarea field
+function arcustomlogin_callback_field_textarea() {
+    echo 'This will be a textarea field';
+}
+
+// Call back: checkbox field
+function arcustomlogin_callback_field_checkbox() {
+    echo 'This will be a checkbox field';
+}
+
+// Call back: select field
+function arcustomlogin_callback_field_select() {
+    echo 'This will be a select field';
+}
